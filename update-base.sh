da51c98db7ec8ff885ecded1f1144108f93a291c
Egor <68809735+ZOrtanin@users.noreply.github.com>
2022-12-03 11:39:01 +0300

    Update update-base.sh

--- update-base.sh
+++ update-base.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
 docker exec -i db mysqladmin -uroot -proot drop wordpress;
 docker exec -i db mysqladmin -uroot -proot create wordpress;
 docker exec -i db mysql -uroot -proot wordpress < wordpress.sql
