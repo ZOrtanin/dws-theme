6a72e528704fc3e49d08259d198f69fea0c1da2a
Egor <68809735+ZOrtanin@users.noreply.github.com>
2022-12-04 16:14:44 +0300

    загрузка плагинов

---
+++ wp-content/plugins/twb-woocommerce-reviews/admin/js/admin_js.js
@@ -0,0 +1,8 @@
+/*
+Plugin Name: TWB Woocommerce Reviews
+Plugin URI: https://theweb-designs.com/
+Version: 1.7.5
+*/
+jQuery(document).ready(function($){
+	 $('.twb-color-picker').wpColorPicker();
+});
